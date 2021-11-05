03e188b70ae3fece59248835e1a648d4ae4d69d5
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-05 14:11:19 -0600

    Implementacion general de recurso "Usuarios". Implementacion de metodo post para crear usuarios en el sistema.

--- app/main/parsers.py
+++ app/main/parsers.py
@@ -3,3 +3,10 @@ from flask_restful import reqparse
 login_put_args = reqparse.RequestParser()
 login_put_args.add_argument( "username", type=str, help="Se requiere un nombre de usuario", required=True )
 login_put_args.add_argument( "password", type=str, help="Se requiere de una contraseña", required= True )
+
+usuario_put_args = reqparse.RequestParser()
+usuario_put_args.add_argument( "nombres", type=str, help="Los nombres son requeridos", required=True )
+usuario_put_args.add_argument( "apellidos", type=str, help="Los apellidos son requeridos", required=True )
+usuario_put_args.add_argument( "nombre_usuario", type=str, help="El nombre de usuario es requerido", required=True )
+usuario_put_args.add_argument( "contrasena", type=str, help="La contraseña es requerida", required=True )
+usuario_put_args.add_argument( "correo_electronico", type=str, help="El correo es requerido", required=True )
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
 from flask import make_response, render_template, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, marshal_with, abort
+from .. import database
 from ..extensions import guard, limiter
+from ..models import *
+from .parsers import *
 
 class MainPage( Resource ):
     def get( self ):
@@ -10,6 +13,17 @@ class MainPage( Resource ):
 class Login( Resource ):
     def post( self ):
         login_args = login_put_args.parse_args()
-        user = guard.authenticate( args[ 'username' ], args[ 'password' ] )
+        user = guard.authenticate( login_args[ 'username' ], login_args[ 'password' ] )
         token = guard.encode_jwt_token( user )
         return jsonify( { 'clave_usuario': user.clave_usuario, 'access_token': token } )
+
+class Usuarios( Resource ):
+    def post( self ):
+        usuario_args = usuario_put_args.parse_args()
+        usuario_existe = Usuario.query.filter_by( nombre_usuario=usuario_args[ 'nombre_usuario' ] ).one_or_none()
+        if usuario_existe:
+            abort( 409, message="El nombre de usuario ya se esta utilizando." )
+
+        usuario = Usuario( nombres=usuario_args[ 'nombres' ], apellidos=usuario_args[ 'apellidos' ], correo_electronico=usuario_args[ 'correo_electronico' ], nombre_usuario=usuario_args[ 'nombre_usuario' ], contrasena=guard.hash_password( usuario_args[ 'contrasena' ] ) )
+        database.session.add( usuario )
+        database.session.commit()
