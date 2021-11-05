4eaa9abd39953743e829d05e593034a0e952074f
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-05 13:34:07 -0600

    Implementacion de recurso "Login"

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,14 @@
-from flask import make_response, render_template
+from flask import make_response, render_template, jsonify
 from flask_restful import Resource
 
 class MainPage( Resource ):
     def get( self ):
         headers = { 'Content-Type': 'Text/html' }
         return make_response( render_template( 'main_page.html' ), 200, headers )
+
+class Login( Resource ):
+    def post( self ):
+        login_args = login_put_args.parse_args()
+        user = guard.authenticate( args[ 'username' ], args[ 'password' ] )
+        token = guard.encode_jwt_token( user )
+        return jsonify( { 'clave_usuario': user.clave_usuario, 'access_token': token } )
