880a697382e48acfa80ba7fdb472ef0b3f12dcdf
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-10 12:28:08 -0600

    Creacion de archivo string_validation, donde se definen metodos que seran utilizados para la validacion de entradas.

---
+++ app/main/string_validation.py
@@ -0,0 +1,16 @@
+import re
+
+def user_input_validation( user_input ):
+    matches = True
+    matches = bool( re.fullmatch( r"/^[a-zA-ZÀ-ÿ\s]{3,40}$/", user_input[ 'nombres' ] ) )
+    matches = bool( re.fullmatch( r"/^[a-zA-ZÀ-ÿ\s]{3,40}$/", user_input[ 'apellidos' ] ) )
+    matches = bool( re.fullmatch( r"/^[a-zA-Z0-9\_\-]{4,25}$/", user_input[ 'nombre_usuario' ] ) )
+    matches = bool( re.fullmatch( r"/^[a-zA-Z0-9\_\-]{4,20}$/", user_input[ 'contrasena' ] ) )
+    matches = bool( re.fullmatch( r"/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/", user_input[ 'correo_electronico' ] ) )
+    return matches
+
+def publication_input_validation( publication_input ):
+    matches = True
+    matches = bool( re.fullmatch( r"/^[a-zA-ZÀ-ÿ\s]{3,40}$/", publication_input[ 'nombre_publicacion' ] ) )
+    matches = bool( re.fullmatch( r"/^[a-zA-ZÀ-ÿ\s\.]{3,200}$/", publication_input[ 'descripcion' ] ) )
+    return matches
